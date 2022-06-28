676486da2a4456df1f17daae8a5a741d232d3a47
ArmSershant <vardges.movsesyan6@gmail.com>
2022-06-28 18:03:36 +0400

    01

---
+++ Homework 1/01.js
@@ -0,0 +1,19 @@
+// Find the value that is not being repeated inside the given array.
+// Գտնել այն արժեքը որը ընհանրապես չի կրկնվում տրված զանգվածում
+// Օրինակ։ ["a", "a", "b", "a"] //Պատասխան։ 'b'
+
+var arr = ['a','a','b','a'];
+var n = arr.length;
+var result = [];
+
+function notRepeatingElem() {
+  for (var i = 0; i < n; i++) {
+    for (var j = 0 ; j < n; j++)
+      if (i != j && arr[i] == arr[j])
+        break;
+    if(j == n)
+        result.push(arr[i]);
+  }
+  return result;
+}
+console.log(notRepeatingElem())
