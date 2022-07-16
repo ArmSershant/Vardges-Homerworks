6215e48f0bb5b92caa3b933464b68d919e0d4b70
Vardges <101866789+ArmSershant@users.noreply.github.com>
2022-07-16 20:19:19 +0400

    08 Solved

---
+++ Homework 2/08-Palindrom.js
@@ -0,0 +1,10 @@
+function checkPalindrom(palindrom) {
+  for (let i = palindrom.length; i > 0; i--) {
+    if ((palindrom[i] = palindrom.charAt(palindrom.length) - 1)) {
+      document.write("the word is palindrome.");
+    } else {
+      document.write("the word is not palindrome!");
+    }
+  }
+}
+checkPalindrom("madam");
